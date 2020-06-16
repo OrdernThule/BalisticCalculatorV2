d22441a9be304c6f1e556712599d52a08718dacf
Freeze <rain-ilia@rambler.ru>
2020-06-16 23:03:20 +0200

    всё работает

--- app/src/main/java/com/example/balisticcalculatorv2/MainActivity.java
+++ app/src/main/java/com/example/balisticcalculatorv2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.balisticcalculatorv2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
@@ -9,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.landing_acitivity_main);
+       // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //navController.navigate(R.id.main_layout);
     }
+
+
+
 }
