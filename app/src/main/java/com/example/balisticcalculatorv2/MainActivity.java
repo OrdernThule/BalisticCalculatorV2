b2b097fed656d50ba68f95fb62b52021d2a1895c
Den <albrechtdurer@ukr.net>
2020-06-19 01:43:55 +0300

    Nav Graph

--- app/src/main/java/com/example/balisticcalculatorv2/MainActivity.java
+++ app/src/main/java/com/example/balisticcalculatorv2/MainActivity.java
@@ -5,16 +5,25 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        setContentView(R.layout.landing_acitivity_main);
+      //  Button viewTransactionsButton = findViewById(R.id.buttonAtmosphere);
        // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        //navController.navigate(R.id.main_layout);
-    }
+        // navController.navigate(R.id.main_layout);
+        //viewTransactionsButton.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+           // public void onClick(View view) {
+             //   Navigation.findNavController(view).navigate(R.id.viewTransactionsAction);
+         //   }
+   //     });
+   }
 
 
 
